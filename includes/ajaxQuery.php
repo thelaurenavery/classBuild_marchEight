9df1d39eb9c367ab4a3566931f1287c24de8f3da
Lauren Avery <laurenavery@Laurens-MacBook-Pro.local>
2017-03-08 11:27:34 -0500

    initial commit

---
+++ includes/ajaxQuery.php
@@ -0,0 +1,21 @@
+<?php
+	include 'config.php';
+
+	$mysqli = new mysqli($config['mysql_server'], $config['mysql_user'], $config['mysql_password'], $config['mysql_db']);
+	if ($mysqli->connect_errno) {
+		printf("Connection failed: %s \n", $mysqli->connect_error);
+		exit();
+	}
+
+	$mysqli->set_charset("utf8");
+
+	// set some post stuff up here
+	$modelNo = $_GET["model"];	
+
+	$myQuery = "SELECT * FROM mainmodel WHERE model = '$modelNo'";
+	$result = mysqli_query($mysqli, $myQuery);
+	$row = mysqli_fetch_assoc($result);	
+
+	echo json_encode($row);
+	//echo mysqli_num_rows($result);			
+?>
